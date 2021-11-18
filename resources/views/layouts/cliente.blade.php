1d1306fd6dcc4fd33c79836b699e70feff2c32ba
L2Ernestoo <ernestoorellana5267@gmail.com>
2021-11-18 15:15:55 -0600

    boleta de etatus

--- resources/views/layouts/cliente.blade.php
+++ resources/views/layouts/cliente.blade.php
@@ -30,6 +30,11 @@
                 <i class="fas fa-fw fa-chart-area"></i>
                 <span>Consultar</span></a>
         </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('revision.index')}}">
+                <i class="fas fa-fw fa-chart-area"></i>
+                <span>Solicitar Revision</span></a>
+        </li>
         <hr class="sidebar-divider d-none d-md-block">
         <div class="text-center d-none d-md-inline">
             <button class="rounded-circle border-0" id="sidebarToggle"></button>
